16e9af400cea9e5d6c18ded368887422d9d0bbeb
nicolaspenagos <nicolas.penagosm98@gmail.com>
2020-11-13 01:52:21 -0500

    Padding problem in editText has been fixed, references of MainActivity have been coded.

--- app/src/main/java/com/example/contacs_and_calling_app/MainActivity.java
+++ app/src/main/java/com/example/contacs_and_calling_app/MainActivity.java
@@ -1,18 +1,56 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author Nicolás Penagos Montoya
+ * nicolas.penagosm98@gmail.com
+ **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
 package com.example.contacs_and_calling_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.FirebaseDatabase;
 
+/*
+* This class controls the screen where the user enters their username at first.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    // -------------------------------------
+    // Database
+    // -------------------------------------
     private FirebaseDatabase database;
 
+    // -------------------------------------
+    // XML references
+    // -------------------------------------
+    private EditText usernameEditText;
+    private Button nextButton;
+
+    // -------------------------------------
+    // Android methods
+    // -------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setOnClickListener(
+
+                (view)->{
+
+                }
+
+        );
+
     }
+
 }
